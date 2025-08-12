027a52bb26949dba5a046a4ec8ad4a06ea2d2f14
bhdspro <bhdspro@gmail.com>
2025-08-12 13:10:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -20,5 +20,6 @@ app.post("/send", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
